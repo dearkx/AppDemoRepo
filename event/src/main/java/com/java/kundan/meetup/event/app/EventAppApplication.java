06ab70b516f168406ca169dfd1650b6add07b6b5
dearkx <kundankx@gmail.com>
2020-08-26 17:19:55 +0530

    Added Eureka Naming Server

--- event/src/main/java/com/java/kundan/meetup/event/app/EventAppApplication.java
+++ event/src/main/java/com/java/kundan/meetup/event/app/EventAppApplication.java
@@ -2,8 +2,10 @@ package com.java.kundan.meetup.event.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EventAppApplication {
 
 	public static void main(String[] args) {
